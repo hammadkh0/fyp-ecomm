c3d64aa13c77078078803ef0d082d6b0d2f7d30b
hammadkh0 <hammad5718@gmail.com>
2023-02-18 13:03:54 +0500

    Feat: Added categories and refactored code
    Categories are scraped with the data and total items are also returned

--- Flask-server/scrape.py
+++ Flask-server/scrape.py
@@ -1,4 +1,3 @@
-from agent_list import AGENT_LIST
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -6,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 
 def intial_config():
@@ -21,18 +19,20 @@ def intial_config():
                               options=options)
     print(options.arguments)
     print("----------------------------------------")
+
     # simulate headless mode by minimizing the window
-    driver.set_window_size(1920, 1080)
+    driver.set_window_size(1366, 768)
     driver.set_window_position(-2000, 0)
     return driver
 
 
-def find_product_list(url, HEADERS, input):
+def find_product_list(url, input):
 
     driver = intial_config()
 
     #load page with beautiful soup
     driver.get(url)
+    #driver.get_screenshot_as_file("screenshot1.png")
 
     # find the search bar and enter the input
     driver.find_element(By.ID, "twotabsearchtextbox").send_keys(input)
@@ -40,17 +40,38 @@ def find_product_list(url, HEADERS, input):
     driver.find_element(By.ID, "nav-search-submit-button").click()
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    driver.get_screenshot_as_file("screenshot3.png")
 
-    #soup2 = BeautifulSoup(driver.page_source, "html.parser")
+    # find all the search results on single page
     output = soup.find_all('div', {'data-component-type': 's-search-result'})
-    print(len(output))
+
+    # wait 1 second for the page to load
+    driver.implicitly_wait(1)
+
+    # find the category div and the list of categories in it
+    departments = soup.find('ul', {'aria-labelledby': 'n-title'})
+    departments = [] if departments == None else departments
+    categories_container = departments.find_all('li',
+                                                {'class': 'a-spacing-micro'})
+    categories_container = [] if categories_container == None else categories_container
+
+    categories = []
+    for item in categories_container:
+        # check if no item has class "a-spacing-micro s-navigation-indent-1" to aoid subcategories
+        # classes are split by whitespaces so check if length is 2
+        if (len(item['class']) == 2):
+            continue
+
+        item2 = item.find('span', {'class': 'a-size-base a-color-base'})
+        item2 = "" if item2 == None else item2.text
+        categories.append(item2)
+
     scraped_items = []
+    # find the data from the search results
     for row in output:
         asin = row['data-asin']
 
-        title = row.find('span',
-                         {"class": "a-size-medium a-color-base a-text-normal"})
+        title = row.find(
+            'span', {"class": "a-size-base-plus a-color-base a-text-normal"})
         title = "" if title == None else title.text
 
         img = row.find('img', {"class": "s-image"})
@@ -63,6 +84,8 @@ def find_product_list(url, HEADERS, input):
             "class": "a-row a-size-small"
         }).find('span', {'class': 'a-icon-alt'})
         rating = "" if rating == None else rating.text
+        # rating = 4.4 out of 5 stars so split by whitespace and take the first element
+        rating = rating.split(" ")[0]
 
         rating_count = row.find('span',
                                 {'class': 'a-size-base s-underline-text'})
@@ -72,16 +95,19 @@ def find_product_list(url, HEADERS, input):
             'data-component-type': 's-product-image'
         }).find('a')['href']
         link = "" if link == None else url + link
+
         data = {
             "asin": asin,
             "title": title,
-            "img": img,
+            "image": img,
             "price": price,
             "rating": rating,
             "rating_count": rating_count,
             "link": link,
+            "categories": categories
         }
         scraped_items.append(data)
 
+    total_items = len(scraped_items)
     driver.quit()
-    return scraped_items
+    return scraped_items, total_items
